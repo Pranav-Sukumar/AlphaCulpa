b344b939d43f8d22be03b0887c356d661befdfb6
jlia904 <jubnliang@gmail.com>
2024-12-15 17:16:39 -0500

    data can now be added from review to json

--- alphaculpa/src/App.js
+++ alphaculpa/src/App.js
@@ -14,13 +14,39 @@ function App() {
   const [data, setData] = useState(initialData);
 
   const [karmaPoints, setKarmaPoints] = useState(50);
+
+  const addReview = (classId, professorName, newReview) => {
+    const updatedData = data.map(cls => {
+      if (cls.classId === classId) {
+        const updatedProfessors = cls.professors.map(prof => {
+          if (prof.name === professorName) {
+            return {
+              ...prof,
+              reviews: {
+                ...prof.reviews,
+                content: [...prof.reviews.content, newReview]
+              }
+            };
+          }
+          return prof;
+        });
+        return { ...cls, professors: updatedProfessors };
+      }
+      return cls;
+    });
+
+    setData(updatedData);
+
+    console.log("Updated JSON:", JSON.stringify(updatedData, null, 2));
+  };
+
   return (
     <Router>
       <Routes>
         <Route path={"/"} element={<Homepage kp = {karmaPoints}/>} />
         <Route path = {"/classsearch"} element = {<ClassSearch kp = {karmaPoints} data = {data}/>} />
         <Route path = {"/classsearch/addclass"} element = {<AddClass kp = {karmaPoints}/>} />
-        <Route path = {"/review"} element = {<WriteReview kp = {karmaPoints} setKP = {setKarmaPoints}/>} />
+        <Route path = {"/review"} element = {<WriteReview kp = {karmaPoints} setKP = {setKarmaPoints} addReview={addReview}/>} />
         <Route path = {"/viewclass/:prof_name"} element = {<ViewInformationClass kp = {karmaPoints} setKP = {setKarmaPoints}/>} />
       </Routes>
 
